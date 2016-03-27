f0b8179592ca0296c6469fea357de4118f9ae715
Vidreven <vidreven@yahoo.com>
2016-03-27 19:03:06 +0200

    Changed method names

--- transaction.rb
+++ transaction.rb
@@ -15,28 +15,28 @@ class Transaction
 
 	def deserialize(tx)
 		obj = {ins: [], outs: []}
-		obj[:version] = @sp.change_endianness(read_and_modify(4, tx))
-		ins = read_var_int(tx)
+		obj[:version] = @sp.change_endianness(read_and_modify!(4, tx))
+		ins = read_var_int!(tx)
 
 		ins.times{
 			obj[:ins] << {
 				outpoint: {
-					hash: @sp.change_endianness(read_and_modify(32, tx)),
-					index: @sp.change_endianness(read_and_modify(4, tx))
+					hash: @sp.change_endianness(read_and_modify!(32, tx)),
+					index: @sp.change_endianness(read_and_modify!(4, tx))
 				},
-				scriptSig: read_var_string(tx),
-				sequence: @sp.change_endianness(read_and_modify(4, tx))
+				scriptSig: read_var_string!(tx),
+				sequence: @sp.change_endianness(read_and_modify!(4, tx))
 			}
 		}
 
-		outs = read_var_int(tx)
+		outs = read_var_int!(tx)
 		outs.times{
 			obj[:outs] << {
-				value: @sp.change_endianness(read_and_modify(8, tx)),
-				scriptPubKey: read_var_string(tx)
+				value: @sp.change_endianness(read_and_modify!(8, tx)),
+				scriptPubKey: read_var_string!(tx)
 			}
 		}
-		obj[:locktime] = @sp.change_endianness(read_and_modify(4, tx))
+		obj[:locktime] = @sp.change_endianness(read_and_modify!(4, tx))
 
 		return obj
 	end
@@ -44,22 +44,22 @@ class Transaction
 	def serialize(txobj)
 		raw = ''
 		raw += @sp.change_endianness(txobj[:version])
-		raw += txobj[:ins].length.to_s(16).rjust(2, '0')
+		raw += txobj[:ins].length.to_s(16).rjust(2, '0') # var_int!
 
 		txobj[:ins].each do |input|
 			raw += @sp.change_endianness(input[:outpoint][:hash])
 			raw += @sp.change_endianness(input[:outpoint][:index])
-			scriptlen = input[:scriptSig].length / 2 # convert charlen to bytelen
+			scriptlen = input[:scriptSig].length / 2 # convert charlen to bytelen var_string!
 			scriptlen = scriptlen.to_s(16)
 			raw += scriptlen + input[:scriptSig]
 			raw += @sp.change_endianness(input[:sequence])
 		end
 
-		raw += txobj[:outs].length.to_s(16).rjust(2, '0')
+		raw += txobj[:outs].length.to_s(16).rjust(2, '0') # var_int!
 
 		txobj[:outs].each do |output|
 			raw += @sp.change_endianness(output[:value])
-			scriptlen = output[:scriptPubKey].length / 2
+			scriptlen = output[:scriptPubKey].length / 2 # var_string!
 			scriptlen = scriptlen.to_s(16)
 			raw += scriptlen + output[:scriptPubKey]
 		end
@@ -223,24 +223,24 @@ class Transaction
 
 	# accepts length in bytes
 	# modifies the string by slicing off bytes
-	def read_and_modify(bytes, tx)
+	def read_and_modify!(bytes, tx)
 		chars = bytes * 2 #reads hexa chars
 		return tx.slice!(0..chars-1)
 	end
 
 	# returns variable part of varint and modyfies tx
-	def read_var_int(tx)
+	def read_var_int!(tx)
 		val = tx.slice!(0..1).to_i(16)
 		return val if val < 253
-		var = read_and_modify(2**(val-252), tx)
+		var = read_and_modify!(2**(val-252), tx)
 		return  @sp.change_endianness(var).to_i(16)
 	end
 
 	# varint + char[]
 	# returns the string and modyfies tx
-	def read_var_string(tx)
-		size = read_var_int(tx)
-		return read_and_modify(size, tx)
+	def read_var_string!(tx)
+		size = read_var_int!(tx)
+		return read_and_modify!(size, tx)
 	end
 
 	def deepcopy(obj)
